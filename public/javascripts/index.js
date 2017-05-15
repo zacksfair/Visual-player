2ea1ae4bdbed461ace0cbbdd59f37c0fadf55cbb
Tony <tony@gmail.com>
2017-05-15 21:50:54 +0800

    second commit

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -15,20 +15,37 @@ for(var i = 0;i < lis.length; i++){
 
 var xhr = new XMLHttpRequest();
 var ac = new (window.AudioContext||window.webkitAudioContext)();
+var gainNode = ac[ac.createGain?"createGain":"createGainNode"]();
+gainNode.connect(ac.destination);
 
+var source = null;
+var count = 0;
 function load(url){
+	var n = ++count;
+	source && source[source.stop?"stop":"noteOff"]();
+	xhr.abort();
   	xhr.open("GET",url);
 	xhr.responseType = "arraybuffer";
 	xhr.onload = function(){
-		debugger
+		if(n != count) return;
 		ac.decodeAudioData(xhr.response,function(buffer){
+			if(n != count) return;
 			var bufferSource = ac.createBufferSource();
 			bufferSource.buffer = buffer;
-			bufferSource.connect(ac.destination);
+			bufferSource.connect(gainNode);
 			bufferSource[bufferSource.start?"start":"noteOn"](0);
+			source = bufferSource;
 		},function(err){
 			console.log(err);
 		})
 	}
 	xhr.send(); 
 }
+
+function changeVolume(percent){
+	gainNode.gain.value = percent*percent;
+}
+$("#volume")[0].onchange = function(){
+	changeVolume(this.value/this.max);
+}
+$("#volume")[0].onchange();
